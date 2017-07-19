761c9b732875d9b3541f50817e5c8d27a018d450
Ricardo Rojas <reddrum7@gmail.com>
2017-07-19 14:19:39 -0500

    Create Vat module in order to abstract logic in other component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,16 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  article = 'Tesla';
-  value: number = 1000;
-  result: number = 0;
-  result2: number = 0;
-
-
-  onClick() {
-    this.result = this.value * 1.19;
-  }
-  onClick2() {
-    this.result2 = this.value * 1.16;
-  }
+  
 }
